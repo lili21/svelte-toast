35c743111d6187119912d783e4fcd96ab55e899d
li.li <li.li@ele.me>
2017-05-08 11:42:49 +0800

    update example

--- example/app.js
+++ example/app.js
@@ -13,7 +13,19 @@ ready(function () {
   demo.addEventListener('click', function (e) {
     e.stopPropagation()
     e.preventDefault()
-    toast.success('Hello Svelte Toast')
+    if (e.target.classList.contains('toast-btn')) {
+      const r = Math.random()
+      // toast.success('Hello Svelte Toast')
+      if (r < 0.25) {
+        toast.show('Hello Svelte Toast -- toast.show')
+      } else if (r < 0.5) {
+        toast.info('Hello Svelte Toast -- toast.info')
+      } else if (r < 0.75) {
+        toast.success('Hello Svelte Toast -- toast.success')
+      } else {
+        toast.error('Hello Svelte Toast -- toast.error')
+      }
+    }
   })
 })
 
